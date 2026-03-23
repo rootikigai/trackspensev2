ee4727c3527705bb3bf9d3e4fd40f59e27dda149
rootikigai <ifykel360@gmail.com>
2026-03-23 17:40:18 +0100

    feat: Initial commit for TrackSpense V.2, main

---
+++ src/main/java/ng/ikigai/trackspensev2/Trackspensev2Application.java
@@ -0,0 +1,13 @@
+package ng.ikigai.trackspensev2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Trackspensev2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Trackspensev2Application.class, args);
+	}
+
+}
