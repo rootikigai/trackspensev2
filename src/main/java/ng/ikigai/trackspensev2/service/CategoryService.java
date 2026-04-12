3d28a0e7606db9f33bf95d714555991b5cef3e31
rootikigai <ifykel360@gmail.com>
2026-04-12 19:11:39 +0100

    feat: implement category management functionality - Create CategoryService with CRUD operations and DTO mapping.

--- src/main/java/ng/ikigai/trackspensev2/service/CategoryService.java
+++ src/main/java/ng/ikigai/trackspensev2/service/CategoryService.java
@@ -1,4 +1,77 @@
 package ng.ikigai.trackspensev2.service;
 
+import lombok.RequiredArgsConstructor;
+import ng.ikigai.trackspensev2.dto.CategoryDTO;
+import ng.ikigai.trackspensev2.entity.CategoryEntity;
+import ng.ikigai.trackspensev2.entity.ProfileEntity;
+import ng.ikigai.trackspensev2.repository.CategoryRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
 public class CategoryService {
+
+    private final ProfileService profileService;
+    private final CategoryRepository categoryRepository;
+
+    //Save Category
+    public CategoryDTO saveCategory(CategoryDTO categoryDTO){
+        ProfileEntity profile = profileService.getCurrentProfile();
+        if (categoryRepository.existsByNameAndProfileId(categoryDTO.getName(), profile.getId())){
+            throw new RuntimeException("Category with this name already exists!");
+        }
+
+        CategoryEntity newCategory = toEntity(categoryDTO, profile);
+        newCategory = categoryRepository.save(newCategory);
+        return toDTO(newCategory);
+    }
+
+    //Get Categories for current user
+    public List<CategoryDTO> getCategoriesForCurrentUser(){
+        ProfileEntity profile = profileService.getCurrentProfile();
+        List<CategoryEntity> categories = categoryRepository.findByProfileId(profile.getId());
+        return categories.stream().map(this::toDTO).toList();
+    }
+
+    //Get categories by type for current user
+    public List<CategoryDTO> getCategoriesByTypeForCurrentUser(String type){
+        ProfileEntity profile = profileService.getCurrentProfile();
+        List<CategoryEntity> entities = categoryRepository.findByTypeAndProfileId(type, profile.getId());
+        return entities.stream().map(this::toDTO).toList();
+    }
+
+    //Update category
+    public CategoryDTO updateCategory(Long categoryId, CategoryDTO dto){
+        ProfileEntity profile = profileService.getCurrentProfile();
+        CategoryEntity existingCategory = categoryRepository.findByIdAndProfileId(categoryId, profile.getId())
+                .orElseThrow(() -> new RuntimeException("Category not found or inaccessible."));
+        existingCategory.setName(dto.getName());
+        existingCategory.setIcon(dto.getIcon());
+        existingCategory = categoryRepository.save(existingCategory);
+        return toDTO(existingCategory);
+    }
+
+    //Helper Methods
+    private CategoryEntity toEntity(CategoryDTO categoryDTO, ProfileEntity profile){
+        return CategoryEntity.builder()
+                .name(categoryDTO.getName())
+                .icon(categoryDTO.getIcon())
+                .profile(profile)
+                .type(categoryDTO.getType())
+                .build();
+    }
+
+    private CategoryDTO toDTO(CategoryEntity entity){
+        return CategoryDTO.builder()
+                .id(entity.getId())
+                .profileId(entity.getProfile() != null ? entity.getProfile().getId(): null)
+                .name(entity.getName())
+                .icon(entity.getIcon())
+                .createdAt(entity.getCreatedAt())
+                .updatedAt(entity.getUpdatedAt())
+                .type(entity.getType())
+                .build();
+    }
 }
