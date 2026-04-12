578cdf267d38337a6beabda084769c6f17b13ae0
rootikigai <ifykel360@gmail.com>
2026-04-12 19:00:20 +0100

    feat: implement category management functionality - Implement CategoryRepository for data access.

--- src/main/java/ng/ikigai/trackspensev2/repository/CategoryRepository.java
+++ src/main/java/ng/ikigai/trackspensev2/repository/CategoryRepository.java
@@ -1,4 +1,23 @@
 package ng.ikigai.trackspensev2.repository;
 
-public interface CategoryRepository {
+import ng.ikigai.trackspensev2.entity.CategoryEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface CategoryRepository extends JpaRepository<CategoryEntity, Long> {
+
+    // SELECT * FROM tbl_categories where profile_id = ?1
+    List<CategoryEntity> findByProfileId(Long profileId);
+
+    //SELECT * FROM tbl_categories where id = ?1 and profile_id = ?2
+    Optional<CategoryEntity> findByIdAndProfileId(Long id, Long profileId);
+
+    //SELECT * FROM tbl_categories where type = ?1 and profile_id = ?2
+    List<CategoryEntity>findByTypeAndProfileId(String type, Long profileId);
+
+    Boolean existsByNameAndProfileId(String name, Long profileId);
+
+    String type(String type);
 }
