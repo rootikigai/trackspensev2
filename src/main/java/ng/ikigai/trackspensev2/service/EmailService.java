f38f096084f30cf175b354327b2b411c8943ff0d
rootikigai <ifykel360@gmail.com>
2026-04-16 22:48:01 +0100

    feat: implement notification service, update email service, and enable scheduling in application

--- src/main/java/ng/ikigai/trackspensev2/service/EmailService.java
+++ src/main/java/ng/ikigai/trackspensev2/service/EmailService.java
@@ -5,6 +5,8 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.mail.MailException;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.mail.javamail.MimeMessagePreparator;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -16,13 +18,26 @@ public class EmailService {
     private String fromEmail;
 
     public void sendEmail(String to, String subject, String body){
+//        try{
+//            SimpleMailMessage message = new SimpleMailMessage();
+//            message.setFrom(fromEmail);
+//            message.setTo(to);
+//            message.setSubject(subject);
+//            message.setText(body);
+//            mailSender.send(message);
+//        }
+//        catch (MailException e){
+//            throw new RuntimeException("Failed to send email to " + to + ": " + e.getMessage(), e);
+//        }
         try{
-            SimpleMailMessage message = new SimpleMailMessage();
-            message.setFrom(fromEmail);
-            message.setTo(to);
-            message.setSubject(subject);
-            message.setText(body);
-            mailSender.send(message);
+            MimeMessagePreparator messagePreparator = mimeMessage -> {
+                MimeMessageHelper helper = new MimeMessageHelper(mimeMessage, "UTF-8");
+                helper.setFrom(fromEmail);
+                helper.setTo(to);
+                helper.setSubject(subject);
+                helper.setText(body, true);
+            };
+            mailSender.send(messagePreparator);
         }
         catch (MailException e){
             throw new RuntimeException("Failed to send email to " + to + ": " + e.getMessage(), e);
