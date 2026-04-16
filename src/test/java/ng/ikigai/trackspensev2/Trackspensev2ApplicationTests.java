30de33ad6e7871b3800d15a470d6429f8a0da0c7
rootikigai <ifykel360@gmail.com>
2026-04-16 22:48:14 +0100

    chore: include project configuration, build files, and resources

---
+++ src/test/java/ng/ikigai/trackspensev2/Trackspensev2ApplicationTests.java
@@ -0,0 +1,13 @@
+package ng.ikigai.trackspensev2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Trackspensev2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
