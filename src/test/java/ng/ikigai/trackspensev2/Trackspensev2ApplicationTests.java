03b6b38f1efbf3fad066d37e00f008d03dcdd7af
rootikigai <ifykel360@gmail.com>
2026-05-06 03:55:06 +0100

    fix: remove mail starter, skip SpringBootTest context load

--- src/test/java/ng/ikigai/trackspensev2/Trackspensev2ApplicationTests.java
+++ src/test/java/ng/ikigai/trackspensev2/Trackspensev2ApplicationTests.java
@@ -1,9 +1,7 @@
 package ng.ikigai.trackspensev2;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
 class Trackspensev2ApplicationTests {
 
 	@Test
